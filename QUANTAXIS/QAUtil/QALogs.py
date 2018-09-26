8694a73d95128a33261b2cac673774455d6390f2
JimmyG <jimmy.m.gong@gmail.com>
2018-09-26 21:17:12 +0800

    trash all logs to /tmp, works for unix-os

--- QUANTAXIS/QAUtil/QALogs.py
+++ QUANTAXIS/QAUtil/QALogs.py
@@ -74,7 +74,7 @@ def get_config():
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s QUANTAXIS>>> %(message)s',
                     datefmt='%H:%M:%S',
-                    filename='{}{}quantaxis-{}-.log'.format(get_config(), os.sep, str(datetime.datetime.now().strftime(
+                    filename='{}{}quantaxis-{}-.log'.format('/tmp', os.sep, str(datetime.datetime.now().strftime(
                         '%Y-%m-%d-%H-%M-%S'))),
                     filemode='w',
                     )
